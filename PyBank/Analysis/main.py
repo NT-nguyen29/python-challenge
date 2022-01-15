6aa64b04033be5218e2b33b070f927f70d72c29c
Tran Nguyen <89397345+NT-nguyen29@users.noreply.github.com>
2022-01-14 20:26:36 -0500

    print analysis, export txt file

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -4,34 +4,50 @@ csvfile = "../Resources/budget_data.csv"
 budget_df = pd.read_csv(csvfile)
 #print(budget_df.head())
 
+# total number of months included in dataset
 totalMonths = budget_df['Date'].count()
+# net total of Profit/Losses
 totalSum = budget_df['Profit/Losses'].sum()
 
-# change = budget_df['Profit/Losses'][i] - budget_df['Profit/Losses'][i - 1]
+# create 'Change' column and set initial value
 budget_df['Change'] = 0
 
+# change = budget_df['Profit/Losses'][i] - budget_df['Profit/Losses'][i - 1]
 for i in range(1, totalMonths):
     change = budget_df['Profit/Losses'][i] - budget_df['Profit/Losses'][i - 1]
     budget_df['Change'][i] = change
-    # change = 0
 
-print(budget_df.head())
-
-# print(averageChange)
+# calculate average of changes in Profit/Losses
 averageChange = round((budget_df['Change'].sum() / (len(budget_df) -1)), 2)
 
 greatIncrease = budget_df['Change'].max()
 greatDecrease = budget_df['Change'].min()
 
-if budget_df['Change'][i] == greatIncrease:
-    increaseDate = budget_df['Date'][i]
-elif budget_df['Change'][i] == greatDecrease:
-    decreaseDate = budget_df['Date'][i]
+# set index to 'Change' and return dates of greatIncrease and greatDecrease
+indexChange_df = budget_df.set_index("Change")
+# print(indexChange_df.head())
 
+increaseDate = list(indexChange_df.loc[{greatIncrease}, "Date"])
+decreaseDate = list(indexChange_df.loc[{greatDecrease}, "Date"])
+
+# print analysis to terminal
+analysis = ["Financial Analysis",
+"________________________",
+f"Total Months: {totalMonths}", 
+f"Total: ${totalSum}",
+f"Average Change: ${averageChange}",
+f"Greatest Increase in Profits: {increaseDate[0]} $({greatIncrease})",
+f"Greatest Decrease in Profits: {decreaseDate[0]} $({greatDecrease})"]
+
+for line in analysis:
+    print(line)
+
+# export analysis to text file
+with open('analysis.txt', 'w') as file:
+    file.write('\n'.join(analysis))
 
 # print(f"Total Months: {totalMonths}")
 # print(f"Total: ${totalSum}")
 # print(f"Average Change: ${averageChange}")
-# print(greatIncrease)
-# print(greatDecrease)
-print(increaseDate)
+# print(f"Greatest Increase in Profits: {increaseDate[0]} $({greatIncrease})")
+# print(f"Greatest Decrease in Profits: {decreaseDate[0]} $({greatDecrease})")
